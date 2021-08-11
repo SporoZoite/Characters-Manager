a8b14dcf7280be2bf220fba031b9547bdd575713
SporoZoite <lisavanhooydonk@gmail.com>
2021-08-11 12:00:47 +0200

    test api

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const app= express()
+const characters = require('./characters.json')
+
+app.get('/characters',(req,res)=>{
+    res.status(200).json(characters)
+})
+
+app.listen(8080,() =>{
+    console.log('I am ready!')
+})
